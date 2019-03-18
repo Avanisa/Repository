91e058bb5328cd08efe20bc52a42b94a9ad8a143
Avanisa <avani.saxena@amdocs.com>
2019-03-18 14:40:26 +0530

    version_2

--- src/test/java/com/qaagility/controller/AppTest.java
+++ src/test/java/com/qaagility/controller/AppTest.java
@@ -2,12 +2,13 @@ package com.qaagility.controller;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import com.qaagility.javaee.Calculator;
+import com.qaagility.javaee.*;
  
 
 public class AppTest {
 
 Calculator myCalc = new Calculator();
+Calcmul Calcmul = new Calcmul();
 
       public void testSlow() {
         System.out.println("slow");
@@ -24,6 +25,13 @@ Calculator myCalc = new Calculator();
 	@Test	
       public void testCalc() {
         assertEquals("Result", 9, myCalc.add(3,6));
+
+
+	
+      public void testcalcmul1 {
+        assertEquals("Result", 9, Calcmul.mul());
       }
 
     }
+
+
--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -2,6 +2,7 @@ package com.qaagility.controller;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import com.qaagility.javaee.*;
  
 public class CalcTest {
 
@@ -23,7 +24,14 @@ public class CalcTest {
       }
 
 	@Test	
-      public void testCalc1() {
+      public void testCal(){
+
+Calcmul testcalmul = new Calcmul();
+Calculator testcaladd = new Calculator();
+
+        assertEquals(testcalmul.mul(),18);
+        assertEquals(testcaladd.add(),9);
+
         assertEquals("Result - Fail", 9, 9);
       }
 	
